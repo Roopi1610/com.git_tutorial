9765d436156d1abbf605b93a5253970496bf4e1a
Roopi1610 <162543455+Roopi1610@users.noreply.github.com>
2024-03-13 16:52:54 +1100

    Create FbTest1.java

---
+++ src/main/java/com/git_tutorial/basics/FbTest1.java
@@ -0,0 +1,36 @@
+package com.git_tutorial.basics;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FbTest {
+	
+	WebDriver driver;
+	@BeforeTest
+	public void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.get("http://www.fb.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+	}
+
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+	}
+
+ @Test
+ public void verifyCreatenewaccount()
+ {
+       System.out.println("Test case 1");
+ }
